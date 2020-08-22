394e4c2326efbf129d18fe9e01879a3115e9681e
Felipeek <felipeek@hotmail.com>
2020-08-22 01:58:46 -0300

    Read from disk

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,10 +1,27 @@
 [org 0x7c00]
 
+mov [BOOT_DRIVE], dl            ; BIOS stores the boot drive in dl
+
 mov bp, 0x8000
 mov sp, bp
 
-push hex_data
-push 4
+;push hex_data
+;push 4
+;call util_16bits_print_hex
+
+mov ah, 0x02            ; read 2 sectors
+mov al, [BOOT_DRIVE]
+push ax
+mov ah, 0x0
+mov al, 0x2
+push ax
+mov ah, 0x0
+push ax
+push 0x9000
+call util_16bits_read_disk_sectors
+
+push 0x9000
+push 1024
 call util_16bits_print_hex
 
 ;push hello_world_string
@@ -15,6 +32,8 @@ call util_16bits_print_hex
 ;add esp, 0x2
 
 jmp $
+
+BOOT_DRIVE: db 0
 
 hex_data:
 db 0xAB, 0xCD, 0x35, 0xF8
@@ -28,3 +47,6 @@ db 'Hello My Beautiful World!', 0
 times 510-($-$$) db 0
 
 dw 0xaa55
+
+times 256 dw 0xDADA
+times 256 dw 0xF1F2
--- src/util_16bits.asm
+++ src/util_16bits.asm
@@ -1,4 +1,56 @@
-; Print N bytes in hex.
+; Read N sectors from disk, using BIOS
+; Arguments (Push order):
+; Number of sectors to read (8 bits) [H]
+; Drive number (8 bits) [L]
+; Track/cylinder number (8 bits) [H]
+; Sector number (8 bits) [L]
+; Head number (8 bits) [H]
+; Padding (8 bits) [L]
+; Buffer address (16 bits) [Note: caller needs to set ES register]
+util_16bits_read_disk_sectors:
+push bp
+mov bp, sp
+
+push ax
+push bx
+push cx
+push dx
+
+mov bx, [bp + 0x4]      ; now bx has the buffer address
+mov ax, [bp + 0x6]      ; now ah has the head number
+mov cx, [bp + 0x8]      ; now ch has the track/cylinder number and cl has the sector number
+mov dx, [bp + 0xA]      ; now dh has the # of sectors and dl has the drive number
+
+mov al, dh              ; BIOS expects # of sectors in al
+mov dh, ah              ; BIOS expects head number in dh
+mov ah, 0x02            ; BIOS expects ah=0x2 and INT 0x13 to read disk sectors
+; BIOS expects track/cylinder number in ch and sector number in cl. They are already correct
+; BIOS expects drive number in dl. It is already correct
+
+int 0x13                ; call BIOS to read the sector
+
+jc util_16bits_read_disk_sectors_error
+
+mov dx, [bp + 0xA]      ; restore number # sectors in dh
+cmp dh, al
+jne util_16bits_read_disk_sectors_error
+
+pop dx
+pop cx
+pop bx
+pop ax
+
+pop bp
+ret
+
+util_16bits_read_disk_sectors_error:
+push DISK_ERROR_MSG
+call util_16bits_print_string
+jmp $
+
+DISK_ERROR_MSG db "Error reading from disk", 0
+
+; Print N bytes in hex, using BIOS
 ; Arguments (Push order):
 ; Pointer to content that should be printed (16 bits)
 ; Number of bytes to be printed (16 bits)
@@ -26,26 +78,26 @@ util_16bits_print_hex_loop:
 cmp cx, 0
 je util_16bits_print_hex_out_loop
 
-mov dl, [bx]
+mov dl, [bx]            ; move next byte to be printed to dl
 
-mov al, dl
-shr al, 4
+mov al, dl              ; move dl to al
+shr al, 4               ; shift right 4 bits, so we can print the most significant nibble first.
 
 cmp al, 0xA
-jge util_16bits_print_hex_letter
+jge util_16bits_print_hex_letter    ; if we are dealing with A, B, C, D, E or F, we need to treat them differently, because of ASCII...
 
-add al, '0'
+add al, '0'             ; in the normal case, we add '0' to the number
 jmp util_16bits_print_hex_int
 
 util_16bits_print_hex_letter:
-sub al, 0xA
-add al, 'A'
+sub al, 0xA             ; in the special ABCDEF case, we first subtract 0xA, so A will become 0, B will become 1, and so on...
+add al, 'A'             ; now we just add 'A'
 
 util_16bits_print_hex_int:
-int 0x10
+int 0x10                ; print using BIOS
 
-mov al, dl
-and al, 0xF
+mov al, dl              ; move dl to al
+and al, 0xF             ; now we print the less significant nibble
 
 cmp al, 0xA
 jge util_16bits_print_hex_letter2
@@ -60,8 +112,8 @@ add al, 'A'
 util_16bits_print_hex_int2:
 int 0x10
 
-dec cx
-inc bx
+dec cx                  ; decrement the number of bytes that need to be printed
+inc bx                  ; increment content's address
 jmp util_16bits_print_hex_loop
 
 util_16bits_print_hex_out_loop:
