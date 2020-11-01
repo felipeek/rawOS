6eda29d18df5136a8bb1e60eca8d14991a314202
Felipeek <felipeek@hotmail.com>
2020-11-01 01:25:11 -0300

    fix: use stdcall for syscall stubs

--- src/asm/syscall_stubs.asm
+++ src/asm/syscall_stubs.asm
@@ -1,9 +1,11 @@
 global syscall_print_stub
 global syscall_print_stub_size
 
+; NOTE: syscall stubs are using stdcall for now
+
 syscall_print_stub:
 	mov eax, 0
 	mov ebx, [esp + 4]
 	int 0x80
-	ret
+	ret 4
 syscall_print_stub_size: dd syscall_print_stub_size - syscall_print_stub
